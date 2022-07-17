17b3447c6e71823a6c262c1e5282df6bc78525cb
tymash <karakash.tymofiy@gmail.com>
2022-07-17 23:19:59 +0300

    Add and fix files controller methods

--- FileStorage.PL/Controllers/FilesController.cs
+++ FileStorage.PL/Controllers/FilesController.cs
@@ -2,6 +2,7 @@ using System.Net;
 using FileStorage.BLL.Models;
 using FileStorage.BLL.Models.FileModels;
 using FileStorage.BLL.Services.Interfaces;
+using FileStorage.BLL.Validation;
 using FileStorage.DAL.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -24,9 +25,11 @@ public class FilesController : ControllerBase
     
     // GET: api/items
     [HttpGet]
-    [Authorize(Roles = "Administrator")]
+    [Authorize]
     public async Task<ActionResult<IEnumerable<FileViewModel>>> GetByFilter([FromQuery] FilterModel filterModel)
     {
+        var userId = _userManager.GetUserId(User);
+        filterModel.UserId = userId;
         var files = await _fileService.GetByFilterAsync(filterModel);
 
         return new ObjectResult(files);
@@ -42,18 +45,39 @@ public class FilesController : ControllerBase
         return new ObjectResult(file);
     }
 
-    // POST: api/items/add
-    [HttpPost("add")]
+    // POST: api/items/upload
+    [HttpPost("upload")]
+    [RequestSizeLimit(500_000_000)]
     [Authorize]
-    public async Task<ActionResult> Add([FromBody] FileCreateModel fileModel)
+    public async Task<IActionResult> Upload()
     {
-        if (User.Identity == null) return Unauthorized();
-        var currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
-        fileModel.UserId = currentUser.Id;
+        var userId = _userManager.GetUserId(User);
+        var files = Request.Form.Files;
+        var filesModels = new List<FileViewModel>();
+        
 
-        var result = await _fileService.AddAsync(fileModel);
+        foreach (var file in files)
+        {
+            filesModels.Add(await _fileService.UploadAsync(userId, file));
+        }
 
-        return new ObjectResult(result);
+        return Ok(filesModels);
+    }
+    
+    [HttpGet("download/{fileId}"), DisableRequestSizeLimit]
+    [Authorize]
+    public async Task<IActionResult> Download(int fileId)
+    {
+        try
+        {
+            var result = await _fileService.DownloadAsync(fileId);
+
+            return File(result.stream, result.contentType, result.fileName);
+        }
+        catch (FileStorageException ex)
+        {
+            return NotFound(ex.Message);
+        }
     }
     
     // PUT: api/items/update/1
@@ -61,7 +85,7 @@ public class FilesController : ControllerBase
     [Authorize]
     public async Task<ActionResult> Update(int itemId, [FromBody] FileEditModel fileModel)
     {
-        fileModel.Id = itemId;
+        // fileModel.Id = itemId;
         var currentUser = await _userManager.FindByNameAsync(User.Identity?.Name);
 
         if (currentUser.Files.All(si => si.Id != itemId) && !await _userManager.IsInRoleAsync(currentUser, "Administrator"))
@@ -73,18 +97,90 @@ public class FilesController : ControllerBase
     }
     
     // POST: api/items/delete/1
-    [HttpPut("delete/{itemId}")]
+    [HttpDelete("delete/{itemId}")]
     [Authorize]
     public async Task<ActionResult> Delete(int itemId)
     {
-        var currentUser = await _userManager.FindByNameAsync(User.Identity?.Name);
+        var userId = _userManager.GetUserId(User);
+        var file = await _fileService.GetByIdAsync(itemId);
 
-        if (currentUser.Files.All(si => si.Id != itemId) && !await _userManager.IsInRoleAsync(currentUser, "Administrator"))
-            return StatusCode((int)HttpStatusCode.Forbidden);
+        if (userId != file.UserId) return Unauthorized();
 
         await _fileService.DeleteAsync(itemId);
 
         return Ok();
     }
     
+    [HttpGet("user")]
+    [Authorize]
+    public async Task<IActionResult> GetByUser()
+    {
+        var userId = _userManager.GetUserId(User);
+
+        var files = await _fileService.GetByUserAsync(userId);
+
+        return new ObjectResult(files);
+    }
+    
+    [Authorize]
+    [HttpPut("{fileId}/recycle")]
+    public async Task<IActionResult> MoveToRecycleBinAsync(int fileId)
+    {
+        var userId = _userManager.GetUserId(User);
+        var file = await _fileService.GetByIdAsync(fileId);
+        if (userId != file.UserId) return Unauthorized();
+
+        try
+        {
+            await _fileService.MoveFileRecycleBinAsync(fileId);
+
+            return Ok();
+        }
+        catch
+        {
+            return NotFound();
+        }
+    }
+    
+    [Authorize]
+    [HttpPut("{fileId}/restore")]
+    public async Task<IActionResult> RestoreFileAsync(int fileId)
+    {
+        var userId = _userManager.GetUserId(User);
+        var file = _fileService.GetByIdAsync(fileId);
+        if (userId != file.Result.UserId) return Unauthorized();
+
+        try
+        {
+            await _fileService.RestoreRecycledFileAsync(fileId);
+
+            return Ok();
+        }
+        catch
+        {
+            return NotFound();
+        }
+    }
+    
+    [Authorize]
+    [HttpPut("{fileId}/changePublic")]
+    public async Task<IActionResult> ChangeVisibilityAsync(int fileId)
+    {
+        var userId = _userManager.GetUserId(User);
+        var file = _fileService.GetByIdAsync(fileId);
+        if (userId != file.Result.UserId) return Unauthorized();
+       
+
+        try
+        {
+            await _fileService.ChangeFileVisibilityAsync(fileId);
+
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
 }
